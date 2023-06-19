b3f86a2bc34efa2e748b49b136fe57483728ad90
Ugaeun <102311958+Planenepgear@users.noreply.github.com>
2023-06-19 17:17:40 +0900

    update 2

--- VoiceAiStudy/Assets/_Projects/Scripts/AnimationTriggerDemo.cs
+++ VoiceAiStudy/Assets/_Projects/Scripts/AnimationTriggerDemo.cs
@@ -13,7 +13,6 @@ namespace Oculus.Voice.Demo.CharacterAnimationDemo
     {
         private Animator animator;
         private int animationSwitchHash;
-        private int animationSwitchHash2;
 
         private void Awake()
         {
@@ -22,28 +21,27 @@ namespace Oculus.Voice.Demo.CharacterAnimationDemo
 
         void Start()
         {
-            animationSwitchHash = Animator.StringToHash("start_greeting");
+            
             
         }
         
         public void UpdateAnimation(string[] values)
         {
-            animationSwitchHash2 = Animator.StringToHash("start_" + values[2]);
-
             if (values[0].Length != 0)
             {
+                animationSwitchHash = Animator.StringToHash("start_" + values[0]);
                 if (values[1] == "Hi")
                 {
                     animator.SetTrigger(animationSwitchHash);
                 }
-
             }
 
             if (values[2].Length != 0)
             {
+                animationSwitchHash = Animator.StringToHash("start_" + values[2]);
                 if (values[3] == "bye")
                 {
-                    animator.SetTrigger(animationSwitchHash2);
+                    animator.SetTrigger(animationSwitchHash);
                 }
             }
         }
